7572e287ee646b87163321d80216c6a829d9c788
David Linares <gdochile@gmail.com>
2026-08-29 12:18:34 -0400

    feat: add tablero public configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+window.TABLERO_CONFIG = Object.freeze({
+  supabaseUrl: 'https://lfdmbkzghnwvsapxypvt.supabase.co',
+  supabaseKey: 'sb_publishable_bRnkA6PA8-v073nrw9zxiQ_8rVGiOn1',
+  dashboardFunction: 'tablero-empresa',
+  adminFunction: 'tablero-admin',
+  inactiveDays: 15,
+  brand: {
+    name: 'Academia Movida SST',
+    slogan: 'De la Reacción a la Prevención.',
+    website: 'https://www.movidasst.com/',
+    academy: 'https://movidasst.org/',
+    logo: 'https://lh3.googleusercontent.com/d/1F1EETURi32f_QWBJSnrU7DOYJ_NeNLK7=s400'
+  }
+});
